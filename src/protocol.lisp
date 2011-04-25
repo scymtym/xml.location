53ff1cc635af613b69a873ea5972fb0d422d850e
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-04-25 22:46:45 +0200

    Cosmetic changes in src/protocol.lisp * src/protocol.lisp (xml->): cosmetic changes   (->xml): likewise

--- src/protocol.lisp
+++ src/protocol.lisp
@@ -171,12 +171,16 @@ arguments but is a sub-type of `location'."))
 ;;; Conversion Protocol
 ;;
 
-(defgeneric xml-> (value type &key inner-types)
+(defgeneric xml-> (value type
+		   &key
+		   inner-types)
   (:documentation
    "Convert VALUE to the type designated by TYPE and, possibly
 INNER-TYPES. The result of the conversion is returned."))
 
-(defgeneric ->xml (value dest type &key inner-types)
+(defgeneric ->xml (value dest type
+		   &key
+		   inner-types)
   (:documentation
    "Convert VALUE to a suitable type and store the result of the
 conversion in the XML node DEST. Should return VALUE."))
