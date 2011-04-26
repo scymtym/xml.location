758b213c2ac2165f398bc28c2e27570251ad41ca
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-04-26 13:35:11 +0200

    Added conversion into matrix instances in src/lisplab.lisp * src/lisplab.lisp (xml-> element matrix-base): new method; conversion   into existing matrix instance   (xml-> element eql matrix-base): use conversion method for instances   (define-matrix-xml->-method): define conversion methods for matrix   subclasses

--- src/lisplab.lisp
+++ src/lisplab.lisp
@@ -24,23 +24,91 @@
 ;;
 
 (defmethod xml-> ((value stp:element)
-		  (type  (eql 'lisplab:matrix-base))
-		  &key
-		  inner-types
-		  &allow-other-keys)
-  "Create a matrix from VALUE by extracting meta-data from attributes
-and reading elements from a text child node."
+		  (type  lisplab:matrix-base)
+		  &key &allow-other-keys)
+  "Store matrix data in VALUE into the matrix instance TYPE. Signal an
+error if dimensions or element type are incompatible."
   (with-locations-r/o (((:@   rows         :type 'number)        ".")
 		       ((:@   cols         :type 'number)        ".")
 		       ((:@   element-type :type 'symbol)        ".") ;; TODO what about (complex double-float)?
 		       ((:val text         :type '(list number)) "text()[last()]"))
       value
 
-    (let* ((class  (%element-type->matrix-class
-		    (or (first inner-types) element-type)))
+    ;; Check compatibility of dimensions.
+    (unless (and (= (lisplab:rows type) rows)
+		 (= (lisplab:cols type) cols))
+      (error 'xml->-conversion-error
+	     :value            value
+	     :type             type
+	     :format-control   "~@<The stored matrix dimensions, ~D x ~
+~D, do not match the dimensions ~D x ~D of the supplied destination ~
+matrix.~@:>"
+	     :format-arguments (list rows cols
+				     (lisplab:rows type)
+				     (lisplab:cols type))))
+
+    ;; Check compatibility of element types.
+    (unless (subtypep element-type (lisplab:element-type type))
+      (error 'xml->-conversion-error
+	     :value            value
+	     :type             type
+	     :format-control   "~@<The stored element type ~S is not a ~
+subtype of the element type ~S of the supplied destination ~
+matrix.~@:>"
+	     :format-arguments (list element-type
+				     (lisplab:element-type type))))
+
+    ;; Check number of matrix elements, import if ok.
+    (let ((elements text))
+      (unless (length= (the non-negative-integer (* rows cols))
+		       elements)
+	(error 'xml->-conversion-error
+	       :value            value
+	       :type             type
+	       :format-control   "~@<The number of stored elements, ~D, ~
+does not match the number of elements, ~D, of the supplied destination ~
+matrix.~@:>"
+	       :format-arguments (list (length elements) (* rows cols))))
+
+      (lisplab:import-list type elements))
+
+    type))
+
+(defmethod xml-> ((value stp:element)
+		  (type  (eql 'lisplab:matrix-base))
+		  &key
+		  inner-types
+		  matrix-class
+		  &allow-other-keys)
+  "Create a matrix from VALUE by extracting meta-data from attributes
+and reading elements from a text child node."
+  (when (and inner-types matrix-class)
+    (error "The keyword parameters ~S and ~S are mutually exclusive."
+	   :inner-types :matrix-class))
+
+  (with-locations-r/o (((:@ rows         :type 'number) ".")
+		       ((:@ cols         :type 'number) ".")
+		       ((:@ element-type :type 'symbol) ".")) ;; TODO what about (complex double-float)?
+      value
+
+    (let* ((class  (or matrix-class
+		       (%element-type->matrix-class
+			(or (first inner-types) element-type))))
 	   (result (lisplab:mnew class 0 rows cols)))
-      (lisplab:import-list result text)
-      result)))
+      (xml-> value result))))
+
+(macrolet ((define-matrix-xml->-method (class)
+	     `(defmethod xml-> ((value stp:element)
+				(type  (eql ',class))
+				&key
+				inner-types
+				&allow-other-keys)
+		(xml-> value 'lisplab:matrix-base
+		       :inner-types  inner-types
+		       :matrix-class ',class))))
+  (define-matrix-xml->-method lisplab:matrix-dge)
+  (define-matrix-xml->-method lisplab:matrix-zge)
+  (define-matrix-xml->-method lisplab:matrix-ge))
 
 
 ;;; -> XML conversion
