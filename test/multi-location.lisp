64a73d174de35d9f95833e6871d9952319599d7a
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-06-05 11:39:17 +0200

    Added test cases for multi-location in test/multi-location.lisp * test/multi-location.lisp (test multi-location-root::smoke): new test   case; test constructing `multi-location' instances   (test multi-location-root::attribute): test attribute access with   `multi-location' instance

--- test/multi-location.lisp
+++ test/multi-location.lisp
@@ -20,9 +20,23 @@
 (in-package :cxml-location.test)
 
 (deftestsuite multi-location-root (root)
-  ((simple-document "<bla a='1'><bli b='5'>umumum</bli><bli/></bla>"))
+  ((simple-document "<bla a='1'><bli b='5'>umumum</bli><bli b='-5'/></bla>"))
   (:documentation
    "Unit tests for the `multi-location' location class."))
+
+(addtest (multi-location-root
+          :documentation
+	  "Smoke test for `multi-location' class.")
+  smoke
+
+  (ensure (loc simple-document "bla/bli/text()"
+	       :if-multiple-matches :all))
+
+  ;; We get a location, its result set is empty
+  (let ((loc (loc simple-document "no-such-element"
+		  :if-multiple-matches :all)))
+    (ensure loc)
+    (ensure (xpath:node-set-empty-p (location-result loc)))))
 
 (addtest (multi-location-root
           :documentation
@@ -38,3 +52,21 @@
 
     (ensure-same (name loc) '("blup" "blup")
 		 :test #'equal)))
+
+(addtest (multi-location-root
+          :documentation
+	  "Test attribute accessor of the `multi-location'
+class.")
+  attribute
+
+  (let ((loc (loc simple-document "/bla/bli"
+		  :if-multiple-matches :all)))
+    (ensure-same (@ loc "b") '("5" "-5")
+		 :test #'equal)
+
+    (setf (@ loc "b") "6")
+    (ensure-same (@ loc "b") '("6" "6")
+		 :test #'equal)
+
+    (ensure-condition 'error ;;; TODO(jmoringe): proper condition
+      (@ loc "attribute-does-not-exist"))))
