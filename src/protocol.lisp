c3c10d5cfb12f3ce2f9c50b8f6ec917ad7b01434
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-02-17 17:21:18 +0100

    Improved documentation in src/protocol.lisp
    * src/protocol.lisp (header): added one-line summary
      (loc): improved documentation string

--- src/protocol.lisp
+++ src/protocol.lisp
@@ -1,4 +1,4 @@
-;;;; protocol.lisp ---
+;;;; protocol.lisp --- Protocol provided by the xml.location system.
 ;;;;
 ;;;; Copyright (C) 2011, 2012, 2013 Jan Moringen
 ;;;;
@@ -6,9 +6,11 @@
 
 (cl:in-package #:xml.location)
 
-
 ;;; Location Protocol
-;;
+;;;
+;;; Location object represent zero, one or more locations such as
+;;; element nodes or attribute nodes in XML documents which are
+;;; described by an XPath expression.
 
 (defgeneric location-document (location)
   (:documentation
@@ -102,9 +104,7 @@ element node. If NAME is a qualified name of the form
 PREFIX:LOCAL-NAME, LOCATION has to contain an entry for PREFIX in its
 namespace table."))
 
-
 ;;; Dynamic Location Class Family
-;;
 
 (define-dynamic-class-family location
     "This dynamic class family consists of all location classes. A
@@ -116,9 +116,7 @@ classes as its superclasses. Typical location mixin classes are:
 + `ignore-empty-results-mixin'"
   (:common-superclasses (location)))
 
-
 ;;; Location Construction Protocol
-;;
 
 (defgeneric loc (document path
 		 &rest args
@@ -129,34 +127,85 @@ classes as its superclasses. Typical location mixin classes are:
 		 assign-mode
 		 &allow-other-keys)
   (:documentation
-   "Construct and return a new `location' instance that represents the
-nodes that result from applying the XPath PATH to the XML document
-DOCUMENT. ARGS are passed to the `make-instance' call that creates the
+   "Construct and return a new location object that represents the
+nodes resulting from applying the XPath PATH to the XML document
+DOCUMENT. ARGS are passed to the `make-instance' call that creates a
 `location' instance.
 
-NAMESPACES can be an alist of the form ((PREFIX . NAMESPACE)*) that
-specifies XML namespaces that should be available in PATH.
+NAMESPACES can be an alist of the form
+
+  ((PREFIX . NAMESPACE)*)
+
+that specifies XML namespaces that should be made available in PATH.
 
 IF-NO-MATCH specifies the policy for dealing with the situation that
 the node set produced by evaluating PATH on DOCUMENT is empty. Valid
-values are :error, :create and :do-nothing.
+values:
+
+:error (the default)
+
+  Signal an `empty-result-set' error.
+
+:create
+
+  Try to create a location which matches PATH in DOCUMENT, then return
+  the created location.
+
+:do-nothing.
+
+  Return a location object that does not have an associated location
+  in document.
 
 IF-MULTIPLE-MATCHES specifies the policy for dealing with the
 situation that the node set produced by evaluating PATH on DOCUMENT
-consists of multiple nodes. Valid values are :error, :first, :any and
-last.
+consists of multiple nodes. Valid values are:
+
+:error (the default)
+
+  Signal a `too-many-matches-in-result-set' error.
+
+:first
+
+  Return a location object corresponding to the first location in
+  DOCUMENT which matches PATH.
+
+:any
+
+  Return a location object corresponding to an arbitrary location in
+  DOCUMENT which matches PATH.
+
+:last.
+
+  Return a location object corresponding to the last location in
+  DOCUMENT which matches PATH.
+
+:all
+
+  Return a location object corresponding to the set of matching
+  locations. Operations on the location object affect all locations
+  simultaneously.
 
 ASSIGN-MODE specifies the semantics of assigning values to `val'
-places of the location. If ASSIGN-MODE is :set, the new value replaces
-the previous value. If ASSIGN-MODE is :append, the new value is stored
-in a newly appended sibling location.
+places of the location:
+
+:set
+
+  An assigned value replaces the previous value.
+
+:append
+
+  Subsequently assigned values are stored in newly appended sibling
+  locations.
 
 The type of the returned `location' instance can depend on the
 arguments but is a sub-type of `location'."))
 
-
 ;;; Conversion Protocol
-;;
+;;;
+;;; This protocol is used to convert between Lisp objects and values
+;;; in XML documents. It is used reading/writing locations with types
+;;; other than `string' and for reading/writing element node locations
+;;; which necessarily require some conversion.
 
 (defgeneric xml-> (value type
 		   &key
@@ -172,9 +221,7 @@ INNER-TYPES. The result of the conversion is returned."))
    "Convert VALUE to a suitable type and store the result of the
 conversion in the XML node DEST. Should return VALUE."))
 
-
-;;; Default behavior
-;;
+;; Default behavior of the conversion protocol
 
 (defmethod xml-> :around ((value t) (type t)
 			  &key
