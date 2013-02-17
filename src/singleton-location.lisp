55bc278f297bcc8231f02343f9035c77185517bd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-02-17 17:07:06 +0100

    Use uninterned symbol in in-package form in src/singleton-location.lisp
    * src/singleton-location.lisp (in-package xml.location): use
      uninterned symbol to designate package

--- src/singleton-location.lisp
+++ src/singleton-location.lisp
@@ -4,7 +4,7 @@
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
-(cl:in-package :xml.location)
+(cl:in-package #:xml.location)
 
 (defclass singleton-location (location)
   ((if-no-match         :initarg  :if-no-match
